calculation mistake: probabilities of 0 requests and c colored nodes must sum to 1 looping over all c

scripts/markov.py
@@ -2,14 +2,14 @@ import math
 import numpy as np
 
 # constants
-k = 3  # how many requests per group of request
-n = 10  # number of nodes
+k = 5  # how many requests per group of request
+n = 20  # number of nodes
 
 # Probabilities : array[i][j] probability of i colored states, j groups of requests
-array = np.ndarray(shape=(n, n), dtype=float)
+array = np.ndarray(shape=(n+1, n+1), dtype=float)
 array.fill(-1)  # table contains -1 if the probability was not yet computed
 
-for i in range(n):
+for i in range(n+1):
     array[0][i] = 0
 
 array[1][1] = 1  # one node receives the information and emits one group of requests
@@ -20,21 +20,13 @@ def Pcreqs(c, reqs):
     print("requested proba of " + str(c) + " colored nodes and " +
           str(reqs) + " groups of requests")
     res = 0
-    if c == n:
-        print("all nodes colored")
-    if reqs == 0:
-        print("end: no requests")
     if c < 1:
-        print("less than 1 colored nodes : impossible")
         return 0
     if c > n:
-        print("more colored nodes than nodes: impossible")
         return 0
     if reqs > c:
-        print("more group of requests than colored nodes: impossible")
         return 0
     if reqs < 0:
-        print("less than 0 requests: impossible")
         return 0
 
     if array[c][reqs] != -1:
@@ -46,13 +38,25 @@ def Pcreqs(c, reqs):
             return 0
         if 0 > k or k > n-1:
             return 0
-        res += math.comb(n-c+t, t) * math.comb(c-t-1, k-t) * \
-            Pcreqs(c-t, reqs+1-t)
-    return res / math.comb(n-1, k)
+
+        if reqs + 1 - t > 0: # no requests can't progress
+            res += math.comb(n-c+t, t) * math.comb(c-t-1, k-t) * \
+                Pcreqs(c-t, reqs+1-t)
+    ans = res / math.comb(n-1, k)
+    array[c][reqs] = ans
+    return ans
 
 
-for i in range(1, n):
-    for j in range(n):
+for i in range(1, n+1):
+    for j in range(n+1):
         array[i][j] = Pcreqs(i, j)
 
+
 print(array)
+
+sumNoRequests = 0
+
+for i in range(0, n+1):
+    sumNoRequests += array[i][0]
+
+print("SUM", sumNoRequests)