ed448b08f5c71fff7987dc04acc112fa409a270a
Nicolas Couraud <sp.couraud.nicolas@gmail.com>
2022-11-01 13:26:42 +0100

    more article

--- scripts/markov.py
+++ scripts/markov.py
@@ -1,63 +1,82 @@
 import math
 import numpy as np
-
-# constants
-k = 12  # how many requests per group of request
-n = 20  # number of nodes
-
-# Probabilities : array[i][j] probability of i colored states, j groups of requests
-array = np.ndarray(shape=(n+1, n+1), dtype=float)
-array.fill(-1)  # table contains -1 if the probability was not yet computed
-
-for i in range(n+1):
-    array[0][i] = 0
-
-array[1][1] = 1  # one node receives the information and emits one group of requests
-print(array)
+from joblib import Parallel, delayed
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
-def Pcreqs(c, reqs):
-    print("requested proba of " + str(c) + " colored nodes and " +
-          str(reqs) + " groups of requests")
-    res = 0
-    if c < 1:
-        return 0
-    if c > n:
-        return 0
-    if reqs > c:
-        return 0
-    if reqs < 0:
-        return 0
+def getNoConsensusProb(n, k):
 
-    if array[c][reqs] != -1:
-        return array[c][reqs]
-    for t in range(k+1):
-        if 0 > k-t or k-t > c-t-1:
-            continue
-        if 0 > t or 0 > n-c:
-            continue
-        if 0 > k or k > n-1:
-            continue
+    array = np.ndarray(shape=(n+1, n+1), dtype=float)
+    array.fill(-1)  # table contains -1 if the probability was not yet computed
+    for i in range(n+1):
+        array[0][i] = 0
 
-        if reqs + 1 - t > 0: # no requests can't progress
-            res += math.comb(n-c+t, t) * math.comb(c-t-1, k-t) * \
-                Pcreqs(c-t, reqs+1-t)
-    ans = res / math.comb(n-1, k)
-    array[c][reqs] = ans
-    return ans
+    # one node receives the information and emits one group of requests
+    array[1][1] = 1
+
+    def Pcreqs(c, reqs):
+        res = 0
+        if c < 1:
+            return 0
+        if c > n:
+            return 0
+        if reqs > c:
+            return 0
+        if reqs < 0:
+            return 0
+
+        if array[c][reqs] != -1:
+            return array[c][reqs]
+        for t in range(k+1):
+            if 0 > k-t or k-t > c-t-1:
+                continue
+            if 0 > t or 0 > n-c:
+                continue
+            if 0 > k or k > n-1:
+                continue
+
+            if reqs + 1 - t > 0:  # no requests can't progress
+                res += math.comb(n-c+t, t) * math.comb(c-t-1, k-t) * \
+                    Pcreqs(c-t, reqs+1-t)
+        ans = res / math.comb(n-1, k)
+        array[c][reqs] = ans
+        return ans
+    noCons = 0
+    for i in range(0, n+1):
+        if i != n:
+            noCons += Pcreqs(i, 0)
+    return noCons
 
 
-for i in range(1, n+1):
-    for j in range(n+1):
-        array[i][j] = Pcreqs(i, j)
+nodes = range(3, 26)
+ks = range(2, 13)
 
 
-print(array)
+def computeAll(n, ks):
+    resn = []
+    for k in ks:
+        if n > k:
+            g = getNoConsensusProb(n, k)
+            if g == 0:
+                resn.append(0)
+            else:
+                resn.append(getNoConsensusProb(n, k))
+        else:
+            resn.append(0)
+    return resn
 
-sumNoRequests = 0
 
-for i in range(0, n+1):
-    sumNoRequests += array[i][0]
+results = Parallel(n_jobs=-1)(delayed(computeAll)(n, ks) for n in nodes)
 
-print("SUM", sumNoRequests)
-print("no consensus", 1-array[n][0])
+X, Y = np.meshgrid(ks, nodes)
+
+fig = plt.figure()
+
+ax = plt.axes(projection="3d")
+ax.contour3D(X, Y, results, 200, cmap="viridis")
+ax.set_xlabel("k")
+ax.set_ylabel("n")
+ax.set_zlabel("epsilon")
+ax.set_title("Probability of not reaching consensus")
+plt.show()
--- scripts/markovBeta.py
+++ scripts/markovBeta.py
@@ -7,41 +7,7 @@ from joblib import Parallel, delayed
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-# constants
-# k = 8  # how many requests per group of request
-# n = 30   # number of nodes
 
-# Probabilities : array[i][j][k][l] probability of i colored nodes, j semi-colored nodes, k requests from colored nodes, l requests from semi-colored nodes
-
-# array = np.ndarray(shape=(n+1, n+1, n+1, n+1), dtype=float)
-# array.fill(-1)  # table contains -1 if the probability was not yet computed
-
-# # one node receives the information and emits one group of requests
-# array[0][1][0][1] = 1
-# print(array)
-
-
-# for i in range(n+1):
-#     for j in range(n+1):
-#         for u in range(n+1):
-#             for v in range(n+1):
-#                 array[i][j][u][v] = Pcreqs(i, j, u, v)
-
-# print(Pcreqs(0, 4, 0, 3))
-# print(Pcreqs(3, 1, 3, 2))
-# print(array)
-
-# sumNoRequests = 0
-# noConsensus = 0
-
-# for i in range(0, n+1):
-#     for j in range(0, n+1):
-#         sumNoRequests += array[i][j][0][0]
-#         if i+j != n:
-#             noConsensus += array[i][j][0][0]
-
-# print("SUM", sumNoRequests)
-# print("no consensus", noConsensus)
 
 
 def getNoConsensusProb(n, k):
@@ -111,34 +77,32 @@ def getNoConsensusProb(n, k):
         # considering a step with a group of requests from a colored node
         # print("considering a step with a group from colored node only if no groups of requests from semi colored nodes")
         resc = 0
-        for tv in range(k+1):
-            if reqss != tv:
-                # no requests from semi colored nodes
+        tv = reqss
+        # no requests from semi colored nodes
+        for ts in range(k+1):
+            if n-c-s+ts+tv < tv:
                 continue
-            for ts in range(k+1):
-                if n-c-s+ts+tv < tv:
-                    continue
-                if s-tv+ts < ts:
-                    continue
-                if k-tv-ts > c-ts-1:
-                    continue
-                if c-ts-1 < 0:
-                    continue
-                if s-tv < 0:
-                    continue
-                if n-c-s+ts+tv < 0:
-                    continue
-                if k - tv-ts < 0:
-                    continue
-                if reqss-tv < 0:
-                    continue
-                if reqsc+1-ts < 0:
-                    continue
+            if s-tv+ts < ts:
+                continue
+            if k-tv-ts > c-ts-1:
+                continue
+            if c-ts-1 < 0:
+                continue
+            if s-tv < 0:
+                continue
+            if n-c-s+ts+tv < 0:
+                continue
+            if k - tv-ts < 0:
+                continue
+            if reqss-tv < 0:
+                continue
+            if reqsc+1-ts < 0:
+                continue
 
-                if reqsc + 1 - ts > 0:  # no requests can't progress
-                    resc += math.comb(n-c-s+tv, tv) * math.comb(s-tv+ts, ts) * \
-                        math.comb(c-ts-1, k-tv-ts) * Pcreqs(c-ts,
-                                                            s-tv+ts, reqsc+1-ts, reqss-tv)
+            if reqsc + 1 - ts > 0:  # no requests can't progress
+                resc += math.comb(n-c-s+tv, tv) * math.comb(s-tv+ts, ts) * \
+                    math.comb(c-ts-1, k-tv-ts) * Pcreqs(c-ts,
+                                                        s-tv+ts, reqsc+1-ts, reqss-tv)
 
         # total probability (conditionnal probability)
 
@@ -160,7 +124,6 @@ def getNoConsensusProb(n, k):
             if i+j != n:
                 noCons += Pcreqs(i, j, 0, 0)
     return noCons
-
 
 nodes = range(3, 26)
 ks = range(2, 13)
